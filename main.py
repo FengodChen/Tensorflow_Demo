2b68acd5db85d4977511a1aa1ee7c5f8e1ef13c8
FengodChen <a1176553293@outlook.com>
2019-07-04 03:55:20 +0800

    添加了注释以及数据生产代码

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 import tensorflow as tf
+from random import random
 
-x_train = tf.placeholder(shape=[3], dtype=tf.float32)
+# 生成数据
+myLen = 10
+
+def getRandomList(length, multi):
+    rList = []
+    for i in range(length):
+        rList.append(random()*multi)
+    return rList
+
+def getTrainData(w_true):
+    data_x = getRandomList(myLen, 100)
+    data_y = 0
+    for i in range(myLen):
+        data_y += w_true[i] * data_x[i]
+    return [data_x, data_y]
+
+w_true = getRandomList(myLen, 1)
+
+
+# 已知的输入输出
+x_train = tf.placeholder(shape=[myLen], dtype=tf.float32)
 y_train = tf.placeholder(shape=[], dtype=tf.float32)
 
-w = tf.Variable(tf.zeros([3]), dtype=tf.float32)
-
+# 定义权重，预测输出函数，误差，训练方向
+w = tf.Variable(tf.zeros([myLen]), dtype=tf.float32)
 y = tf.reduce_sum(x_train*w)
 loss = tf.abs(y-y_train)
-
 optimizer = tf.train.RMSPropOptimizer(0.001)
 train = optimizer.minimize(loss)
 
+# 初始化训练
 init = tf.global_variables_initializer()
-
 sess = tf.Session()
 sess.run(init)
-for i in range(50000):
-  loss_value = sess.run([x_train, w, loss, train, y], feed_dict={x_train:[1, 2, 3], y_train: 2})
-  if(i % 1000 == 0):
-    print(loss_value)
+
+#开始训练
+loss_value = 0
+for i in range(500):
+    myData = getTrainData(w_true)
+    for j in range(500):
+        loss_value = sess.run([x_train, w, loss, train, y], feed_dict={x_train: myData[0], y_train: myData[1]})
+    print("i = %d, j = %d:" %(i, j))
+    print("loss = %f" %(loss_value[2]))
+    print("w = ")
+    print(loss_value[1])
+
+print("w_true = ")
+print(w_true)
