54cc740e308d5ba81ccabaa250bc51888515e782
FengodChen <a1176553293@outlook.com>
2019-07-31 16:31:05 +0800

    将nameprop变为字典形式

--- Project/keras_SSD300/main_fit.py
+++ Project/keras_SSD300/main_fit.py
@@ -12,32 +12,25 @@ from ssd_utils import BBoxUtility
 from ssd_fit import MultiboxLoss
 from ssd_model import SSD300
 
+priors = pickle.load(open('prior_boxes_ssd300.pkl', 'rb'))
+
 class VOC_Generator():
-    def __init__(self, voc_path, classes_list):
+    def __init__(self, voc_path, classes_list, input_shape):
+        self.input_shape = input_shape
         self.classes_num = len(classes_list)
         self.classes_list = classes_list
-        self.classes_inf_name = []
-        self.classes_inf_prop = []
+        self.classes_inf_nameprop = {}
+        self.bbox = BBoxUtility(self.classes_num, pickle.load(open('prior_boxes_ssd300.pkl', 'rb')))
 
-        for ptr_class in range(self.classes_num):
-            inf_name_tmp = []
-            inf_prop_tmp = []
-            
-            classes_list_listFileName = classes_list[ptr_class]
-            classes_list_listFilePath = voc_path + '/ImageSets/Main/' + classes_list_listFileName + '_train.txt'
+        for class_name in self.classes_list:
+            class_inf_nameprop = {}
+            classes_list_listFilePath = voc_path + '/ImageSets/Main/' + class_name + '_train.txt'
             classes_list_listFile = open(classes_list_listFilePath)
 
             for nameProp in classes_list_listFile:
                 nameProp_split = nameProp.split()
-                inf_name_tmp.append(nameProp_split[0])
-                inf_prop_tmp.append(nameProp_split[1])
+                class_inf_nameprop[nameProp_split[0]] = nameProp_split[1]
             
-            self.classes_inf_name.append(inf_name_tmp)
-            self.classes_inf_prop.append(inf_prop_tmp)
-
-# 初始化值
-NUM_CLASSES = 1
-INPUT_SHAPE = (300, 300, 3)
-
-priors = pickle.load(open('prior_boxes_ssd300.pkl', 'rb'))
-bbox_util = BBoxUtility(NUM_CLASSES, priors)
+            self.classes_inf_nameprop[class_name] = class_inf_nameprop
+            
+#bbox_util = BBoxUtility(NUM_CLASSES, priors)
