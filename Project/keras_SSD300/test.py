b470b53aaf4fbc3d8f058ffb57d48e7c48a6f21e
FengodChen <a1176553293@outlook.com>
2019-08-02 00:33:40 +0800

    test.py添加更多注释

--- Project/keras_SSD300/test.py
+++ Project/keras_SSD300/test.py
@@ -3,11 +3,15 @@ import pickle
 import numpy as np
 
 # p.shape = (7308, 8)
+# p[i] = [xmin, ymin, xmax, ymax, varxc, varyc, varw, varh].
+# Always (varxc, varyc, varw, varh) = (0.1, 0.1, 0.2, 0.2)
 p = pickle.load(open('prior_boxes_ssd300.pkl', 'rb'))
-# gt[key].shape = (x, 7), x means it may 0 or 1 or 2 or another positive integer
+# gt[key].shape = (x, 7)
+# x means box number
+# 7 means classes_number + 4, classes number not include background number
 gt = pickle.load(open('gt_pascal.pkl', 'rb'))
 y = gt['frame04467.png'].copy()
-# Class number = 4
+# Class number = 4, include background class
 bb = ssd_utils.BBoxUtility(4, p)
 # yy.shape = (7308, 16)
 yy = bb.assign_boxes(y)
@@ -17,7 +21,7 @@ targets.append(yy)
 tmp_targets = np.array(targets)
 results = bb.detection_out(tmp_targets)
 # label
-# results[0][:, 0]
+# results[i][:, 0]
 # conf
 # results[i][:, 1]
 # xmin
